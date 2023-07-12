3b5b02e259709df080af3598261ca44cd7ec98e3
royal <824259085@qq.com>
2023-07-12 15:09:54 +0800

    cycle

--- src/main/java/com/github/sample/config/SecurityConfiguration.java
+++ src/main/java/com/github/sample/config/SecurityConfiguration.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
@@ -17,8 +19,8 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request
-                .requestMatchers("/sms/send/code", "/callback/payment").permitAll()
-                .anyRequest().authenticated());
+                .requestMatchers("/engine-rest", "/engine-rest/**", "/callback/payment", "/actuator/**").permitAll()
+                .anyRequest().permitAll());
         return http.build();
     }
 
