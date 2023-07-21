53a82d4f2f79737baa824f618250abd1b24b2b4f
royal <824259085@qq.com>
2023-07-21 09:50:13 +0800

    fix timer start event error

--- src/main/java/com/github/sample/GoodCamundaApp.java
+++ src/main/java/com/github/sample/GoodCamundaApp.java
@@ -1,9 +1,11 @@
 package com.github.sample;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class GoodCamundaApp {
 
     public static void main(String[] args) {
